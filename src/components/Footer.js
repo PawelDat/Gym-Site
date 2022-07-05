04c8fad00a82d63a3619e3c8438fb694757d4196
PawelDat <pawel.datko2000@gmail.com>
2022-07-05 16:09:23 +0200

    Completed Footer Section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box mt="80px" bgcolor="#"
+    alignItems="center"
+    >
+      <Stack
+      flexDirection="row"
+        gap="40px"
+        px="40px"
+        pt="24px"
+        pb="20px"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <img src={Logo} alt="Gym Logo" width="100px" height="100px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Gym Club
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
